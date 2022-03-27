8726c90e01c512b62994bdd629b9108987dd7f01
Gabeh98 <bielhenry09@gmail.com>
2022-03-27 18:04:31 -0300

    adicionando validacao usuario

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -4,8 +4,11 @@ import moment from 'moment';
 import Text from '../Text';
 import EditIcon from '../Icons/EditIcon';
 import DeleteIcon from '../Icons/DeleteIcon';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../redux';
 export default function PostCard(props: OwnProps) {
   const { title, content, username, created_datetime, onDelete, onEdit } = props;
+  const loggedUser = useSelector((state: RootState) => state.auth.name);
   return (
     <Wrapper>
       <Header>
@@ -13,8 +16,12 @@ export default function PostCard(props: OwnProps) {
           {title}
         </Text>
         <ActionsButtons>
-          <EditIcon onClick={onEdit} />
-          <DeleteIcon onClick={onDelete} />
+          {loggedUser === username ? (
+            <>
+              <EditIcon onClick={onEdit} />
+              <DeleteIcon onClick={onDelete} />
+            </>
+          ) : null}
         </ActionsButtons>
       </Header>
       <UserInfo>
